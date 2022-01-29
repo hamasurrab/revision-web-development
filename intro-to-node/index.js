7b9af86781ed70a07a0e48a200f713f8b28097e6
hamasurrab <83877518+hamasurrab@users.noreply.github.com>
2022-01-30 05:06:45 +0530

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,13 @@
+// const fs= require("fs");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+
+var superheroes=require("superheroes");
+// var mySuperHeroName =superheroes.random();
+// console.log(mySuperHeroName);
+// _________or______________________________________
+console.log(superheroes.random());
+
+
+var supervillains=require("supervillains");
+console.log(supervillains.random());
